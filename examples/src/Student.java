ab9b5e750f76818e6ce9e57cbfd48b5958a10a7f
sevdaolgunn <sevdaolgun111@hotmail.com>
2022-10-19 19:21:37 +0300

    commit

---
+++ examples/src/Student.java
@@ -0,0 +1,13 @@
+
+    public class Student {
+        String username;
+        int ID;
+
+        Student (String username, int ID){
+            this.username = username;
+            this.ID = ID;
+        }
+
+    }
+
+
