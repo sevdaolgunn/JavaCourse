89c583157599083cfa1128568f15257dae13490e
sevdaolgunn <sevdaolgun111@hotmail.com>
2022-11-17 10:57:49 +0300

    calling a constructor from constructor

---
+++ labs/lab6/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Person person1 = new Person("Sevda", "OLGUN", 20, 1234);
+
+        //person1.getAll();
+
+        Person person2 = new Person("Sevda", "OLGUN");
+       // person2.getAll();
+
+        Person person3 = new Person(1234);
+        // person3.getAll();
+
+        Person person4 =new Person();
+        //person4.getAll();
+
+
+        Person[] persons = {person1,person2,person3,person4};
+        for (Person p : persons)
+            p.getAll();
+
+
+    }
+}
---
+++ labs/lab6/src/Person.java
@@ -0,0 +1,36 @@
+public class Person {
+    String firstName;
+    String lastName;
+    int id;
+    int age;
+
+
+    Person(String firstName, String lastName, int age, int id){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+        this.age = age;
+    }
+
+
+    Person(String firstName, String lastName){
+
+        this(firstName,lastName,0,0);
+        this.id++;
+    }
+
+    Person(int id){
+        this.id = id;
+    }
+
+    Person (){
+        this.id++;
+    }
+
+    public void getAll(){
+        System.out.println(firstName + "  " + lastName + "  "+ id + "  "+ age);
+    }
+
+
+
+}
